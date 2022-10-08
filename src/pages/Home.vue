8a144a829c30dd8e093d69725513e44284924b25
phuet1 <112240579+phuet1@users.noreply.github.com>
2022-10-08 22:17:40 +0700

    Add files via upload

---
+++ src/pages/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="home">
+
+    <div class="image">
+        <img src="images/mob.png" alt="">
+    </div>
+
+    <div class="content">
+        <h3>hi, {{name}}</h3>
+        <span>{{motto}}</span>
+        <p>{{content}}</p>
+        <a href="about.html" class="btn"> about me <i class="fas fa-user"></i> </a>
+    </div>
+
+</section>
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue'
+export default {
+  components: { Navbar },
+  data() {
+      return {
+        name: 'Phuet Phongkheaw',
+        motto: 'great power comes with great responsibility',
+        content: 'Hi',
+      }
+ }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,41 @@
+import { createRouter , createWebHistory } from "vue-router";
+import Home from "../pages/Home.vue"
+import About from"../pages/About.vue"
+import Projects from"../pages/Projects.vue"
+import Contact from"../pages/Contact.vue"
+
+const routes =[{
+    path:"/",
+    component: Home,
+    name: "Home",
+},{
+    path:"/about",
+    component: About,
+    name: "About",
+},{
+    path:"/projects",
+    component: Projects,
+    name: "Projects",
+},{
+    path:"/contact",
+    component: Contact,
+    name: "Contact",
+}]
+
+
+const router = createRouter({
+    history: createWebHistory(),
+    linkActiveClass: "active",
+    scrollBehavior(to, from, savePosition) {
+        if (to.hash) {
+            return {
+                selector: to,hash,
+                behavior: 'smooth'
+            };
+        }
+        return {x: 0, y:0 };
+    },
+    routes,
+});
+
+export default router;
